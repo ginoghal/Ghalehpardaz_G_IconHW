e477228f4348be241799893f8bded6d9d97969f2
Gino Ghalehpardaz <ginoghalehpardaz@Ginos-MacBook-Pro-2.local>
2019-05-24 02:45:49 -0400

    added all icons and began javascript

--- js/main.js
+++ js/main.js
@@ -1,3 +1,28 @@
+(() =>{
 
-let name = 'Gino';
+
 console.log(name);
+
+var= document.getElementById('myButton');
+
+ //add event listener
+ myBtn.addEventListener('click', function(viewBox) {
+  "Water Type are super effective against Fire types";
+
+
+  function logMyId() {
+    console.log(this.id);
+
+    this.style.opacity = 0.5;
+
+
+
+    function viewBox(){
+      water_Icon.textContent = "Now you're something else!!";
+      water_Icon.classList.toggle("selected")
+      subHead.textContent = "You are also something else!";
+
+    }
+
+
+})();
